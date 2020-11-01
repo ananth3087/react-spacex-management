006a2fdca53af70a9d84d652acf6c0436d3e7396
ananth <ananth.3087@gmail.com>
2020-11-02 03:32:10 +0530

    Modified responsive behavior

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -51,43 +51,47 @@ const Home = (props) => {
   return (
     <div className="home">
       <div className="sidebar">
-        <h5 className="sidebar__title">{Constants.FILTERS.TITLE}</h5>
-        <div className="sidebar__filter-item">
-          <CustomFilter
-            label={Constants.FILTERS.LAUNCH_YEAR.LABEL}
-            items={Constants.FILTERS.LAUNCH_YEAR.ITEMS}
-            type={Constants.FILTERS.LAUNCH_YEAR.TYPE}
-            handleFilter={handleFilter}
-            selectedValue={props.launch_year}
-          />
-        </div>
-        <div className="sidebar__filter-item">
-          <CustomFilter
-            label={Constants.FILTERS.SUCCESSFUL_LAUNCH.LABEL}
-            items={Constants.FILTERS.SUCCESSFUL_LAUNCH.ITEMS}
-            type={Constants.FILTERS.SUCCESSFUL_LAUNCH.TYPE}
-            handleFilter={handleFilter}
-            selectedValue={props.launch_success}
-          />
-        </div>
-        <div className="sidebar__filter-item">
-          <CustomFilter
-            label={Constants.FILTERS.SUCCESSFUL_LANDING.LABEL}
-            items={Constants.FILTERS.SUCCESSFUL_LANDING.ITEMS}
-            type={Constants.FILTERS.SUCCESSFUL_LANDING.TYPE}
-            handleFilter={handleFilter}
-            selectedValue={props.land_success}
-          />
+        <div className="sidebar__wrapper">
+          <h5 className="sidebar__title">{Constants.FILTERS.TITLE}</h5>
+          <div className="sidebar__filter-item">
+            <CustomFilter
+              label={Constants.FILTERS.LAUNCH_YEAR.LABEL}
+              items={Constants.FILTERS.LAUNCH_YEAR.ITEMS}
+              type={Constants.FILTERS.LAUNCH_YEAR.TYPE}
+              handleFilter={handleFilter}
+              selectedValue={props.launch_year}
+            />
+          </div>
+          <div className="sidebar__filter-item">
+            <CustomFilter
+              label={Constants.FILTERS.SUCCESSFUL_LAUNCH.LABEL}
+              items={Constants.FILTERS.SUCCESSFUL_LAUNCH.ITEMS}
+              type={Constants.FILTERS.SUCCESSFUL_LAUNCH.TYPE}
+              handleFilter={handleFilter}
+              selectedValue={props.launch_success}
+            />
+          </div>
+          <div className="sidebar__filter-item">
+            <CustomFilter
+              label={Constants.FILTERS.SUCCESSFUL_LANDING.LABEL}
+              items={Constants.FILTERS.SUCCESSFUL_LANDING.ITEMS}
+              type={Constants.FILTERS.SUCCESSFUL_LANDING.TYPE}
+              handleFilter={handleFilter}
+              selectedValue={props.land_success}
+            />
+          </div>
         </div>
       </div>
       <div className="home__content">
-        {launches &&
-          launches.length > 0 &&
-          launches.map((item, index) => {
-            const key = `Launches-${index}`;
-            if (index > 30) return; // testing purpose to avoid loadin more data
-            return <Launch launch={item} key={key} />;
-          })}
+        <div className="home__content-wrapper">
+          {launches &&
+            launches.length > 0 &&
+            launches.map((item, index) => {
+              const key = `Launches-${index}`;
+              if (index > 30) return; // testing purpose to avoid loadin more data
+              return <Launch launch={item} key={key} />;
+            })}
+        </div>
       </div>
     </div>
   );
